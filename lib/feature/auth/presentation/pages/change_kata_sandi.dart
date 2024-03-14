43922c32dfbe7dcaa23883a3976968cbabab205e
ninikynrsh <e41222753@student.polije.ac.id>
2024-03-14 13:49:00 +0700

    update

---
+++ lib/feature/auth/presentation/pages/change_kata_sandi.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class lupaKataSandi extends StatefulWidget {
+  const lupaKataSandi({super.key});
+
+  @override
+  State<lupaKataSandi> createState() => _lupaKataSandiState();
+}
+
+class _lupaKataSandiState extends State<lupaKataSandi> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
